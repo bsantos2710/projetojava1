9ec176d15e8535546d6718826b97ed42583f7977
bcs27 <bcs27@DESKTOP-2H9FIFK>
2021-08-19 22:05:52 -0300

    Alteração e criação da class pessoa

---
+++ projetojava1/src/curso/java/Pessoa.java
@@ -0,0 +1,21 @@
+package curso.java;
+
+public class Pessoa {
+
+	private String nome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	private String sobreNome;
+	
+	
+}
